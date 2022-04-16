fab0ae3a12b31adff453cd693d69d626bf7d9214
MosQyy <akbarma2002@gmail.com>
2022-04-16 16:33:44 +0700

    fix lint on component

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+      },
+    ],
+  },
 };
--- src/screens/bottomNav/HomeScreen/components/RecommendBooks.js
+++ src/screens/bottomNav/HomeScreen/components/RecommendBooks.js
@@ -11,7 +11,7 @@ const RecommendBooks = props => {
 
   useEffect(() => {
     if (data.length > 0) {
-      setList(prevState => [...data]);
+      setList([...data]);
     }
   }, [data]);
 
