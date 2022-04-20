53e85085bd31a1d1a1aebb808032e198acb729e9
MosQyy <akbarma2002@gmail.com>
2022-04-20 20:43:18 +0700

    repair services

--- jestSetupFile.js
+++ jestSetupFile.js
@@ -5,3 +5,12 @@ jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
 jest.mock('react-native-share', () => {});
 jest.mock('react-native-blob-util', () => {});
 jest.mock('@react-native-community/netinfo', () => {});
+jest.mock('redux-persist', () => {
+  const real = jest.requireActual('redux-persist');
+  return {
+    ...real,
+    persistReducer: jest
+      .fn()
+      .mockImplementation((config, reducers) => reducers),
+  };
+});
